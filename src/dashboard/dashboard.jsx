7630cf2b376d4f603f7370b63c342a00e8b7051f
ReginaldoPL <reginaldo.pedro.de.lima@gmail.com>
2018-08-23 13:10:48 -0300

    Redux - state vinculado

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -9,17 +10,20 @@ import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render(){
+        //pegando os valores do redux
+        const { credit, debt} = this.props.summary
         return(
+            
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
                     <Row>
                         <ValueBox cols='12 4' color='green' icon='bank'
-                             value='R$ 10' text = 'Total de Créditos' />
+                             value={`R$ ${credit}`} text = 'Total de Créditos' />
                         <ValueBox cols='12 4' color='red' icon='credit-card'
-                              value='R$ 10' text = 'Total de Débitos' />
+                              value={`R$ ${debt}`} text = 'Total de Débitos' />
                         <ValueBox cols='12 4' color='blue' icon='money'
-                             value='R$ 0' text = 'Valor Consolidado' />
+                             value={`R$ ${credit - debt}`} text = 'Valor Consolidado' />
                     </Row>
                 </Content>
 
@@ -28,4 +32,6 @@ class Dashboard extends Component {
         )
     }
 }
-export default Dashboard
+///vincular props do objeto atual com o do Redux
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+export default connect(mapStateToProps)(Dashboard)
